2cd06d104e3b1fa0d74bdee3d1c6789ace1d8d9d
Nirweint <nirweint_1@mail.ru>
2021-06-20 18:30:29 +0300

    add input component

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import Context from "../context";
 
@@ -18,7 +18,7 @@ const styles = {
 };
 
 function TodoItem({ todo, index, onChange }) {
-    const {removeTodo} = useContext(Context)
+    const { removeTodo } = useContext(Context);
     const classes = [];
 
     if (todo.completed) {
@@ -37,7 +37,9 @@ function TodoItem({ todo, index, onChange }) {
                 &nbsp;
                 {todo.title}
             </span>
-            <button className="rm" onClick={removeTodo.bind(null, todo.id)}>&times;</button>
+            <button className="rm" onClick={removeTodo.bind(null, todo.id)}>
+                &times;
+            </button>
         </li>
     );
 }
